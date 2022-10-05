1f6a352fd8fe01ed57969860a9bfb22a5ebb9b73
teonasnetto <teonasnetto@gmail.com>
2022-10-05 20:38:15 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/teonas/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teonas.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
