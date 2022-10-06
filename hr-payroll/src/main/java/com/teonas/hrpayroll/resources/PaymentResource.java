e167a4d57e9de1ed970015ad8df30c018db398db
teonasnetto <teonasnetto@gmail.com>
2022-10-06 19:08:04 -0300

    hr-payroll implementation and mock

---
+++ hr-payroll/src/main/java/com/teonas/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,27 @@
+package com.teonas.hrpayroll.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teonas.hrpayroll.services.PaymentService;
+
+import entities.Payment;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResource {
+
+    @Autowired
+    private PaymentService service;
+
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+    
+}
---
+++ hr-payroll/src/main/java/entities/Payment.java
@@ -0,0 +1,49 @@
+package entities;
+
+import java.io.Serializable;
+
+public class Payment implements Serializable{
+    
+    private String name;
+    private Double dailyincome;
+    private Integer days;
+
+    public Payment() {
+    }
+
+    public Payment(String name, Double dailyincome, Integer days) {
+        this.name = name;
+        this.dailyincome = dailyincome;
+        this.days = days;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyincome() {
+        return dailyincome;
+    }
+
+    public void setDailyincome(Double dailyincome) {
+        this.dailyincome = dailyincome;
+    }
+
+    public Integer getDays() {
+        return days;
+    }
+
+    public void setDays(Integer days) {
+        this.days = days;
+    }
+
+    public Double getTotal() {
+        return days * dailyincome;
+    }
+
+    
+}
