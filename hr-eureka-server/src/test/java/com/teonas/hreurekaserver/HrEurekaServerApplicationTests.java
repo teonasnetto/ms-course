21aa3f18baac8331d375cf168f1dd69f313f092b
teonasnetto <teonasnetto@gmail.com>
2022-10-06 21:57:22 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/teonas/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teonas.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
