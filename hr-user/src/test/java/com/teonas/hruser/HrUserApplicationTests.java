dc29b8de4da2d854329a945481ba74d1ee43df2d
Teonas Netto <teonasnetto@gmail.com>
2022-10-15 15:06:30 -0300

    Projetc hr-user created

---
+++ hr-user/src/test/java/com/teonas/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teonas.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
