25a27dd9613f260f1c28f2e99f6346851a2f53e6
Teonas Netto <teonasnetto@gmail.com>
2022-10-15 14:53:33 -0300

    Private Git repository

--- hr-config-server/src/main/java/com/teonas/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/teonas/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,25 @@
 package com.teonas.hrconfigserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
+
+	// @Value("${spring.cloud.config.server.git.username}")
+	// private String username;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// System.out.println("USERNAME = " + username);
+
 	}
 
 }
