b55adeb87a909c852aff7f51217092dcbd65eb33
Teonas Netto <teonasnetto@gmail.com>
2022-10-15 15:10:36 -0300

    Configuring hr-user project

--- hr-user/src/main/java/com/teonas/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/teonas/hruser/HrUserApplication.java
@@ -2,7 +2,9 @@ package com.teonas.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
