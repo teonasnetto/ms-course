df507ec7b6f6d1d7f6c91f55b6e255e27006a4d3
Teonas Netto <teonasnetto@gmail.com>
2022-10-15 13:49:47 -0300

    hr-config-server created

---
+++ hr-config-server/src/test/java/com/teonas/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teonas.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
