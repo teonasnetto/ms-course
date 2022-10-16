4eec93f2a6a500dc23ca75d26c298674be1c21ca
Teonas Netto <teonasnetto@gmail.com>
2022-10-16 16:03:46 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/teonas/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/teonas/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.teonas.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.teonas.hroauth.entities.User;
 import com.teonas.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -23,6 +26,17 @@ public class UserService {
             throw new IllegalArgumentException("Email not found");
         }
         logger.info("Email found: " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+        if (user == null) {
+            logger.error("username not found: " + username);
+            throw new UsernameNotFoundException("username not found");
+        }
+        logger.info("username found: " + username);
         return user;
     }
 }
